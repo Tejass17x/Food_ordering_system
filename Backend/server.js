5a6817faeb89269518d1215fc836bce016158c59
Your Name <tejindia7789@gmail.com>
2026-06-28 01:45:48 +0530

    Update API configuration and deployment settings

--- Backend/server.js
+++ Backend/server.js
@@ -4,85 +4,43 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 
-
 const authRoutes = require("./routes/authRoutes");
 const restaurantRoutes = require("./routes/restaurantRoutes");
 
-
-
 const app = express();
 
-
-// Database
 connectDB();
 
-
-
-// CORS
 app.use(
-    cors({
-        origin:[
-            "http://localhost:5173",
-            "http://localhost:5174"
-        ],
-        credentials:true
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://food-ordering-system-virid.vercel.app",
+    ],
+    credentials: true,
+  })
 );
 
-
-
-// Body parser
 app.use(express.json());
 
 app.use(
-    express.urlencoded({
-        extended:true
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
-
-
-
-// Test Route
-
-app.get("/",(req,res)=>{
-
-    res.json({
-
-        message:"Food Ordering Backend Running 🚀"
-
-    });
-
+app.get("/", (req, res) => {
+  res.json({
+    message: "Food Ordering Backend Running 🚀",
+  });
 });
 
-
-
-
-// API Routes
-
-app.use(
-    "/api/auth",
-    authRoutes
-);
-
-
-app.use(
-    "/api/restaurants",
-    restaurantRoutes
-);
-
-
-
-
-// Server
+app.use("/api/auth", authRoutes);
+app.use("/api/restaurants", restaurantRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,()=>{
-
-    console.log(
-        `Server running on port ${PORT}`
-    );
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
