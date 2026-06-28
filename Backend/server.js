4928d250bce9f28a357694c073de566252966170
Your Name <tejindia7789@gmail.com>
2026-06-28 23:18:39 +0530

    fix server express import

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const restaurantRoutes = require("./routes/restaurantRoutes");
+
+
 const app = express();
 
 
@@ -45,3 +55,25 @@ app.use(
 // Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Food Ordering Backend Running 🚀"
+  });
+});
+
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/restaurants", restaurantRoutes);
+
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
