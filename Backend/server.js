de147ec8637780b427913a5c35340d1742cb1837
Your Name <tejindia7789@gmail.com>
2026-06-28 23:04:53 +0530

    Fix cors issue

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const restaurantRoutes = require("./routes/restaurantRoutes");
 
+
 const app = express();
 
 
@@ -14,35 +15,35 @@ const app = express();
 connectDB();
 
 
-// CORS
-app.use(
-  cors({
-    origin: "https://food-ordering-system-pink.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+// CORS FIX
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://food-ordering-system-pink.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization"
+    ],
     credentials: true
-  })
-);
+}));
 
 
-// Preflight
-app.options("*", cors());
-
-
-// Body Parser
+// Body parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-// Test Route
-app.get("/", (req, res) => {
-  res.json({
-    message: "Food Ordering Backend Running 🚀"
-  });
-});
+app.use(express.urlencoded({extended:true}));
 
 
 // Routes
+app.get("/", (req,res)=>{
+    res.json({
+        message:"Food Ordering Backend Running 🚀"
+    });
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/restaurants", restaurantRoutes);
 
@@ -50,6 +51,6 @@ app.use("/api/restaurants", restaurantRoutes);
 // Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
 });
